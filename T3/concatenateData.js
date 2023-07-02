1ed89c499f966705f567a9d262b86d97e0b64f4b
KonstantinChikov <kmpetrov7@gmail.com>
2023-07-02 21:55:28 +0300

    Day I

--- T3/concatenateData.js
+++ T3/concatenateData.js
@@ -1,3 +1,6 @@
+// Ex. VI --> Напишете функция, която получава: име, фамилия, възраст и град и печата съобщение от следния вид:
+// "You are <firstName> <lastName>, a <age>-years old person from <town>."
+
 function concatenateData(input){
 
     let firstName = input[0];
--- T3/greetingByName.js
+++ T3/greetingByName.js
@@ -1,3 +1,6 @@
+// Ex. V --> Напишете функция, която получава име на човек и отпечатва "Hello, <name>!",
+// където <name> е въведеното име от конзолата.
+
 function greetingByName(input){
 
     let name = input[0]
